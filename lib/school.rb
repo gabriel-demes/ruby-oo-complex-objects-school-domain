1b7fa38abdaa8d64bd15208eff6ab2422de67fe9
gabriel-demes <Gabrieldemes@nyu.edu>
2021-01-12 13:45:10 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+
+    attr_accessor :name,:roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if roster[grade]
+            roster[grade].push(student)
+        else 
+            roster[grade] = [student]
+        end
+    end
+
+    def grade(yr)
+        @roster[yr]
+    end
+
+    def sort
+        @roster.each {|grade,student|
+        @roster[grade] = student.sort}
+    end
+
+end
